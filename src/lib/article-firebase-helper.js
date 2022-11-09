1a671c4b629c072e93d9c6babd2309e38231ebc1
zFunx <20762882+zFunx@users.noreply.github.com>
2022-11-09 05:34:14 +0530

    Check if title already exists

--- src/lib/article-firebase-helper.js
+++ src/lib/article-firebase-helper.js
@@ -1,12 +1,31 @@
 import { checkIfDocumentExists, setDocument } from './firebase-helper'
+import Toastify from 'toastify-js'
 
-export function createArticle({db, title, description}) {
-    setDocument({
-        db, 
-        collectionName: 'articles',
-        docName: title,
-        docData: {
-            title, description
-        },
-    })
+export async function createArticle({ db, title, description }) {
+    const collectionName = 'articles';
+    const docName = title.trim().replace(/[^A-Za-z0-9 ]/g, "").replace(/ /g, "-");
+    if (await checkIfDocumentExists(db, collectionName, docName)) {
+        Toastify({
+            text: 'Title already exists',
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+                background: "red",
+            },
+        }).showToast()
+    } else {
+        setDocument({
+            db,
+            collectionName,
+            docName: title,
+            docData: {
+                title, description
+            },
+        })
+    }
+
 }
